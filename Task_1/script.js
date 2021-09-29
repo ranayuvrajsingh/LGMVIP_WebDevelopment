80ce6e0c98b7250fe8e183f8e0422bac652b43ee
ranayuvrajsingh <49943077+ranayuvrajsingh@users.noreply.github.com>
2021-09-29 16:27:42 +0530

    Add files via upload

---
+++ Task_1/script.js
@@ -0,0 +1,19 @@
+let thumbnail = document.getElementsByClassName('thumbnail');
+let activeImages = document.getElementsByClassName('active');
+
+for (var i=0; i < thumbnail.length; i++){
+  thumbnail[i].addEventListener('mouseover', function(){
+    document.getElementById('featured').src = this.src;
+  })
+}
+
+let buttonRight = document.getElementById('slideRight');
+let buttonLeft = document.getElementById('slideLeft');
+
+buttonLeft.addEventListener('click', function(){
+  document.getElementById('slider').scrollLeft -= 80;
+})
+
+buttonRight.addEventListener('click', function(){
+  document.getElementById('slider').scrollLeft += 80;
+})
