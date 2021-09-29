d90e6ca37b3b5215f7c5851df691fc04df1ce97e
ranayuvrajsingh <49943077+ranayuvrajsingh@users.noreply.github.com>
2021-09-29 17:15:44 +0530

    Add files via upload

---
+++ Task_2/src/components/Loader.js
@@ -0,0 +1,7 @@
+const Loader = ({ show }) => {
+    return (
+        (show) ?
+            <div className="loader"></div> : <h1>Welcome!</h1>
+    )
+}
+export default Loader;
---
+++ Task_2/src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = ({ onSetIsClicked }) => {
+    const clicked = () => {
+        onSetIsClicked(1);
+    }
+    return (
+        <div className="Navbar">
+            <ul>
+                <li><a className="brand">LetsGrowMore</a></li>
+                <button onClick={clicked}className="button button1">Get Data</button>
+            </ul>
+        </div>
+    )
+}
+export default Navbar;
